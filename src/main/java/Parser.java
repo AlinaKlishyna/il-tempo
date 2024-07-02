cdb6f172e29ade5ee442b538336a4ad3c0d27af8
Alina Klishyna <alinakotova102@gmail.com>
2024-07-02 11:40:03 +0200

    comments

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -9,32 +9,42 @@ import java.time.Year;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+/**
+ * @author Klishyna ALina
+ */
 public class Parser {
+
+    private static String date = "";
 
     public static void main(String[] args) throws IOException {
         Document page = getPage();
-
         //css query language
-        Element containerDates = page.select("div[class=forecast-container]").first();
-        System.out.println(containerDates);
 
-        System.out.println(getDate(containerDates));
-        ;
+        System.out.println(getDate(page));
     }
 
+    /**
+     * Скачивание/получение страницы сайта
+     */
     private static Document getPage() throws IOException {
-        String url = "https://www.meteolive.it/previsione-meteo/italia/Lombardia/Milano/settimana/";
+        final String url = "https://www.meteolive.it/previsione-meteo/italia/Lombardia/Milano/settimana/";
         //3000-время обработки в миллисекундах, сайт же не будет грузиться вечно
-        return Jsoup.parse(new URL(url), 4000);
+        return Jsoup.parse(new URL(url), 3500);
     }
 
-    private static String getDate(Element containerDates) {
+    /**
+     * Получение 1-ой(текущей) даты в списке
+     * @param page
+     * @return date
+     */
+    private static String getDate(Document page) {
+        Element containerDates = page.select("div[class=forecast-container]").first();
         Element currentDate = containerDates.select("li[class=\"\"]").first();
         String dateDM = currentDate.select("span").last().text();
         String[] date = (dateDM.replaceAll("/", " ") + " " + Year.now()).split(" ");
 
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE, d MMMM yyyy");
         Calendar calendar = new GregorianCalendar(Integer.parseInt(date[2]), (Integer.parseInt(date[1]) - 1), Integer.parseInt(date[0]));
-        return simpleDateFormat.format(calendar.getTime());
+        return Parser.date = simpleDateFormat.format(calendar.getTime());
     }
 }
